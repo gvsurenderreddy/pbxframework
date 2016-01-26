babd102884d9c7ae4b199fe58b150c4f133ce8fc
John Knight <john@ringfree.biz>
2016-01-26 18:12:04 -0500

    Module Import: asterisk-cli
    Module Import: asterisk-cli

---
+++ 13.0/asterisk-cli/assets/js/asterisk-cli.js
@@ -0,0 +1,19 @@
+//Because I press enter by habit
+$("#astcmd").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#send").click();
+    }
+});
+
+$('#send').click(function(){
+	var dat = $('#astcmd').val();
+  $('div.output').html("<pre>"+ _("Loading...") +"</pre>");
+	$.get("/admin/ajax.php?module=asterisk-cli&command=clicmd&data=" + dat,function(data,status){
+		if(data.status) {
+			var reply = JSON.parse(data.message);
+			$('div.output').html("<pre>"+ reply +"</pre>");
+		}else{
+			$('div.output').html("<b>Error</b>");
+		}
+	});
+});
