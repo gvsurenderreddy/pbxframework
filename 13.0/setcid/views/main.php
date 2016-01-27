3546154155e0e7f3cb9bcfc93152930794c3134a
John Knight <john@ringfree.biz>
2016-01-26 19:13:43 -0500

    Module Import: setcid
    Module Import: setcid

---
+++ 13.0/setcid/views/main.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12">
+			<h1><?php echo _("Set CID")?></h1>
+			<div class="fpbx-container">
+				<?php echo $content?>
+			</div>
+		</div>
+	</div>
+</div>
